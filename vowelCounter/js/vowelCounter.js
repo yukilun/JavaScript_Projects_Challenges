f8bbc27a91b99432ccffbc80c216b1f3edf43d58
yukilun <102889935+yukilun@users.noreply.github.com>
2022-04-05 01:48:25 -0700

    update vowel counter

--- vowelCounter/js/vowelCounter.js
+++ vowelCounter/js/vowelCounter.js
@@ -3,14 +3,39 @@ var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"];
 
 function countVowel(){
     var inputText = document.getElementById("inputText").value;
+    var result = document.getElementById("result");
+
 
     if(inputText==""){
         window.alert("Please enter a piece of text!");
     }
     else{
         var count = 0;
-        inputText.split("").forEach(element => {for(var i=0; i<vowels.length; i++){if(element == vowels[i]) count+=1;}});
-        window.alert("There are "+count+" vowel(s)!");
+        var stringHTML ="<span id='resultString'>";
+        
+
+        inputText.split("").forEach(element => {
+
+            var isVowel = Boolean(false);
+
+            for(var i=0; i<vowels.length; i++){
+                if(element == vowels[i]){
+                    isVowel = true;
+                }
+            }
+
+            if(isVowel){
+                stringHTML += "<b>" + element + "</b>";
+                count++;
+            }
+            else{
+                stringHTML += element;
+            }
+
+        });
+
+        stringHTML += "</span>";
+        result.innerHTML = "There are <b>"+count+"</b> vowels!</br></br>" +  stringHTML;
     }
 
 }
