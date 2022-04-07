0e7c98f82b0a9a46fa8d656a209449d744bb98b1
yukilun <102889935+yukilun@users.noreply.github.com>
2022-04-07 16:17:25 -0700

    Update photogallery3D.js

--- photoGallery3D/js/photogallery3D.js
+++ photoGallery3D/js/photogallery3D.js
@@ -1,25 +1,25 @@
 var currentIndex = 3;
 var numberofPhoto = 7;
+var currentWindowWidth;
+
 
 var container = document.querySelector("div#container");
 
-setImages();
 displayImages();
 
-window.addEventListener('resize', ()=>{
-    setImages();
-    displayImages();
-});
+window.addEventListener('resize', waitAndResize);
 
-window.addEventListener('keydown', event=>{
-    if(event.key == 'F12'){
-        setTimeout(()=> window.location.href = window.location.href, 500);
+function waitAndResize(){
+
+    if (window.outerWidth == currentWindowWidth){
+      setTimeout(waitAndResize,100);
+    } else {
+        displayImages();
     }
-})
+}
 
 function showImg(imgId){
     currentIndex = parseInt(imgId.slice(-2));
-    setImages();
     displayImages();
 }
 
@@ -38,26 +38,16 @@ function showNextImg(e,num){
     else{
         currentIndex = newIndex;
     }
-    setImages();
+
     displayImages();
+
     setTimeout(()=>{e.target.classList.remove("buttonClick");}, 100);
 }
 
-function setImages(){
-    container.innerHTML = 
-    "<img src='https://picsum.photos/id/213/500/500' alt='' id='img00' onclick='showImg(this.id)'>"
-    + "<img src='https://picsum.photos/id/1016/500/500' alt='' id='img01' onclick='showImg(this.id)'>"
-    + "<img src='https://picsum.photos/id/1018/500/500' alt='' id='img02' onclick='showImg(this.id)'>"
-    + "<img src='https://picsum.photos/id/1015/500/500' alt='' id='img03' onclick='showImg(this.id)'>"
-    + "<img src='https://picsum.photos/id/1043/500/500' alt='' id='img04' onclick='showImg(this.id)'>"
-    + "<img src='https://picsum.photos/id/110/500/500' alt='' id='img05' onclick='showImg(this.id)'>"
-    + "<img src='https://picsum.photos/id/199/500/500' alt='' id='img06' onclick='showImg(this.id)'>";   
-}
-
-
 function displayImages(){
+    currentWindowWidth = window.outerWidth;
     var images = document.querySelectorAll("img");
-    var totalWidth = Math.min(window.outerWidth * 0.9, 1300);
+    var totalWidth = Math.min(currentWindowWidth * 0.9, 1300);
     var showPhotoWidth = Math.min(totalWidth * 0.7, 500);
     var showPhotoHeight = 500/500 *  showPhotoWidth;
     var otherPhotoDisplayWidth = (totalWidth - showPhotoWidth) / (numberofPhoto - 1);
@@ -69,16 +59,18 @@ function displayImages(){
     for(var i = 0; i<numberofPhoto; i++){
 
         images[i].className = "";
+        images[i].style.left = "";
+        images[i].style.right = "";
         
         if(i - currentIndex < 0){
             images[i].classList.add('imgleft');
-            images[i].style.left = (i * otherPhotoDisplayWidth) + "px";
+            images[i].style.left = (i * otherPhotoDisplayWidth) + "px";    
             images[i].style.zIndex = imgZIndex;
             imgZIndex++;
         }
         else if(i - currentIndex == 0){
             images[i].classList.add('imgshow');
-            images[i].style.left = (i * otherPhotoDisplayWidth) + "px";
+            images[i].style.left = (i * otherPhotoDisplayWidth) + "px";          
             images[i].style.zIndex = imgZIndex;
             imgZIndex--;
         }
