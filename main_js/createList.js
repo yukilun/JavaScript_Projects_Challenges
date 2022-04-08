7e4d8830acaf04534e4474977ed11244187676e8
yukilun <102889935+yukilun@users.noreply.github.com>
2022-04-07 18:07:36 -0700

    update the project List and photos

--- main_js/createList.js
+++ main_js/createList.js
@@ -1,56 +1,57 @@
 const projects = [
-    {  
+    {
         number: 1,
-        name: 'New Year Countdown',
-        url: 'countdown/',
-        img: 'main_img/countdown.png'
-    },{
-        number: 2,
-        name: 'Matching Game',
-        url: 'matchingGame/',
-        img: 'main_img/matchingGame.png'
-    },{
-        number: 3,
-        name: 'Notes Taker',
-        url: 'noteTakerApp/',
-        img: 'main_img/noteTakerApp.png'
-    }
-    ,{
-        number: 4,
-        name: 'Quiz App',
-        url: 'quizApp/',
-        img: 'main_img/quizApp.png'
-    }
-    ,{
-        number: 5,
-        name: 'To Do List',
-        url: 'toDoList/',
-        img: 'main_img/toDoList.png'
-    }
-    ,{
-        number: 6,
-        name: 'Vowel Counter',
-        url: 'vowelCounter/',
-        img: 'main_img/vowelCounter.png'
-    }
-    ,{
-        number: 7,
         name: 'Mood Calendar',
         url: 'moodCalendar/',
         img: 'main_img/moodCalendar.png'
     }
     ,{
-        number: 8,
+        number: 2,
         name: 'Cat Eye Moving',
         url: 'catEyesMoving/',
         img: 'main_img/catEyesMoving.png'
     }
     ,{
-        number: 9,
+        number: 3,
         name: 'Photo Gallery 3D',
         url: 'photoGallery3D/',
         img: 'main_img/photoGallery3D.png'
     }
+    ,{  
+        number: 4,
+        name: 'Matching Game',
+        url: 'matchingGame/',
+        img: 'main_img/matchingGame.png'
+    },{
+        number: 5,
+        name: 'New Year Countdown',
+        url: 'countdown/',
+        img: 'main_img/countdown.png'
+    },{
+        number: 6,
+        name: 'Notes Taker',
+        url: 'noteTakerApp/',
+        img: 'main_img/noteTakerApp.png'
+    }
+    ,{
+        number: 7,
+        name: 'Quiz App',
+        url: 'quizApp/',
+        img: 'main_img/quizApp.png'
+    }
+    ,{
+        number: 8,
+        name: 'To Do List',
+        url: 'toDoList/',
+        img: 'main_img/toDoList.png'
+    }
+    ,{
+        number: 9,
+        name: 'Vowel Counter',
+        url: 'vowelCounter/',
+        img: 'main_img/vowelCounter.png'
+    }
+
 ];
 
 var projectList = document.getElementById("projectList");
